f7cfab001f00c16feb30780f451b6fe0f36f0301
ángel <abr4xas@users.noreply.github.com>
2015-06-08 21:08:29 -0430

    Update header.php

--- header.php
+++ header.php
@@ -3,15 +3,64 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-
-    <link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
+    <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/icons/logo.png">
+    <link type="text/plain" rel="socialmedia" href="/socialmedia.txt">
+    <link rel="dns-prefetch" href="{{ SITEURL }}"/>
+    <link rel="dns-prefetch" href="//www.gstatic.com"/>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com"/>
+    <link rel="dns-prefetch" href="//fonts.googleapis.com"/>
+    <link rel="dns-prefetch" href="//accounts.google.com"/>
+    <link rel="dns-prefetch" href="//ssl.google-analytics.com"/>
+    <link rel="dns-prefetch" href="//www.google-analytics.com"/>
+    <link rel="dns-prefetch" href="//googleads.g.doubleclick.net"/>
+    <link rel="dns-prefetch" href="//pagead2.googlesyndication.com"/>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="application-name" content="<?php bloginfo('name'); ?>"/>
+    <meta name="msapplication-starturl" content="http:{{ SITEURL }}"/>
+    <meta name="msapplication-tooltip" content="{{ TAG_LINE }}"/>
+    <meta name="msapplication-window" content="width=1024;height=768"/>
+    <meta name="msapplication-task" content="name={{ SITENAME }};action-uri=http:{{ SITEURL }};icon-uri={{ SITEURL }}/theme/images/favicon.ico"/>
+    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-144x144.png"/>
+    <meta name="msapplication-TileColor" content="#1F1F21"/>    
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="imagetoolbar" content="no">
+    <meta http-equiv="pragma" content="cache">
+    <meta http-equiv="cache-control" content="cache">
+    <meta http-equiv="vary" content="content-language">
+    <meta http-equiv="Cache-control" content="max-age=2592000, public">
+    <meta http-equiv="content-style-type" content="text/css">
+    <meta name="application-name" content="<?php bloginfo('name'); ?>">
     <meta name="description" content="<?php bloginfo('description'); ?>">
-
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="medium" content="mult">
+    <meta name="adblock" content="disable">
+    <meta name="rating" content="General">
+    <meta name="resource-type" content="document">
+    <meta name="revisit-after" content="1 days">
+    <meta name="revisit" content="1">
+    <meta name="robots" content="index,follow">
+    <meta name="no-email-collection" content="http://www.unspam.com/noemailcollection/">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-57x57.png">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-196x196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/iconsfavicon.ico">
     <?php wp_head(); ?>
     <script>
     // conditionizr.com
